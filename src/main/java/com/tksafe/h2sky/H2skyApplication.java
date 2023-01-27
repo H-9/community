873b73b95e51a58216fd07c76c9fb3b8682824ed
H-9 <133963939@qq.com>
2023-01-27 16:43:46 +0800

    init repo

---
+++ src/main/java/com/tksafe/h2sky/H2skyApplication.java
@@ -0,0 +1,14 @@
+package com.tksafe.h2sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class H2skyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2skyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tksafe/h2sky/H2skyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tksafe.h2sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2skyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
